15aecc70ed4c9b25ceb789232b6c1d8fc0e0f763
Bhumika V <bhumikavshetty07@gmail.com>
2026-02-28 17:03:50 +0530

    greates number

---
+++ Numbers/003.py
@@ -0,0 +1,10 @@
+#Greatest of 3 integers
+a=84
+b=50
+c=30
+if a>b and a>c:
+    print(f"{a} is greatest number")
+elif b>a and b>c:
+    print(f"{b} is greatest number")
+else:
+    print(f"{c} is greates number")
