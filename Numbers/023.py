d490156b44f9eb429d1a2c3a2ad0a8453e2c928d
Bhumika V <bhumikavshetty07@gmail.com>
2026-03-10 22:44:47 +0530

    square cube and square root

---
+++ Numbers/023.py
@@ -0,0 +1,11 @@
+#To calculate the square of a given number
+num=15
+print(num**2)
+
+#To calculate the cube of a given number
+num=3
+print(num**3)
+
+#To calculate the square root of a given number
+num=25
+print(num**0.5)
